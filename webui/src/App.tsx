149ecbfa66c2d69e19396c62235e67c2b223d118
Sascha <GlancingMind@outlook.com>
2021-02-23 16:03:07 +0100

    Add button to toggle between light- and dark-mode

--- webui/src/App.tsx
+++ webui/src/App.tsx
@@ -1,17 +1,23 @@
 import React from 'react';
 import { Route, Switch } from 'react-router';
+import { BrowserRouter } from 'react-router-dom';
 
+import Themer from './components/Themer';
 import Layout from './layout';
 import BugPage from './pages/bug';
 import ListPage from './pages/list';
 
 export default function App() {
   return (
-    <Layout>
-      <Switch>
-        <Route path="/" exact component={ListPage} />
-        <Route path="/bug/:id" exact component={BugPage} />
-      </Switch>
-    </Layout>
+    <Themer>
+      <BrowserRouter>
+        <Layout>
+          <Switch>
+            <Route path="/" exact component={ListPage} />
+            <Route path="/bug/:id" exact component={BugPage} />
+          </Switch>
+        </Layout>
+      </BrowserRouter>
+    </Themer>
   );
 }
