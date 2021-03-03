1743b2e03118837b56cd46973316a7187253d2ff
Sascha <GlancingMind@outlook.com>
2021-03-03 14:28:45 +0100

    Revert resturcturing between App.tsx and index.tsx
    This should prevent unnecessary merge conflicts.

--- webui/src/App.tsx
+++ webui/src/App.tsx
@@ -1,24 +1,17 @@
 import React from 'react';
 import { Route, Switch } from 'react-router';
-import { BrowserRouter } from 'react-router-dom';
 
-import Themer from './components/Themer';
 import Layout from './layout';
 import BugPage from './pages/bug';
 import ListPage from './pages/list';
-import { defaultLightTheme, defaultDarkTheme } from './themes/index';
 
 export default function App() {
   return (
-    <Themer lightTheme={defaultLightTheme} darkTheme={defaultDarkTheme}>
-      <BrowserRouter>
-        <Layout>
-          <Switch>
-            <Route path="/" exact component={ListPage} />
-            <Route path="/bug/:id" exact component={BugPage} />
-          </Switch>
-        </Layout>
-      </BrowserRouter>
-    </Themer>
+    <Layout>
+      <Switch>
+        <Route path="/" exact component={ListPage} />
+        <Route path="/bug/:id" exact component={BugPage} />
+      </Switch>
+    </Layout>
   );
 }
--- webui/src/layout/Header.tsx
+++ webui/src/layout/Header.tsx
@@ -5,7 +5,7 @@ import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import { makeStyles } from '@material-ui/core/styles';
 
-import { LightSwitch } from 'src/components/Themer';
+import { LightSwitch } from '../components/Themer';
 
 import CurrentIdentity from './CurrentIdentity';
 
