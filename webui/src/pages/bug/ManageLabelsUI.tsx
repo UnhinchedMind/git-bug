f444756687044fe1624f838440d90c2bcf21a733
Tim Becker <tim.becker@mni.thm.de>
2021-03-05 15:27:23 +0100

    first draft of labels via materialui-chips #8

--- webui/src/pages/bug/ManageLabelsUI.tsx
+++ webui/src/pages/bug/ManageLabelsUI.tsx
@@ -1,9 +1,15 @@
 import React, { useEffect, useState } from 'react';
 
+import { Chip, rgbToHex } from '@material-ui/core';
+import { common } from '@material-ui/core/colors';
+import { getContrastRatio } from '@material-ui/core/styles/colorManipulator';
+
+import { Color } from 'src/gqlTypes';
+
 function searchLabel(labels: string[], search: string): Array<any> {
   if (search === '') return labels;
-  return labels.filter((l: string) =>
-    l.toLowerCase().includes(search.toLocaleLowerCase())
+  return labels.filter((l: any) =>
+    l.name.toLowerCase().includes(search.toLocaleLowerCase())
   );
 }
 
@@ -20,6 +26,14 @@ function QueryLabels(): Array<any> {
   console.log(query[0].R + ', ' + query[0].G + ', ' + query[0].B);
 
   return query;
+  rgb:
+          'rgb(' +
+          l.R.toString +
+          ',' +
+          l.G.toString +
+          ',' +
+          l.B.toString +
+          ')',
 } */
 
 type Props = {
@@ -30,7 +44,12 @@ function ManageLabelsUI({ queriedlabels }: Props) {
   let [searchInput, setSearch] = useState('');
   let [labels, setLabels] = useState(
     queriedlabels.map((l: any) => {
-      return l.name;
+      return {
+        name: l.name,
+        R: l.color.R,
+        G: l.color.G,
+        B: l.color.B,
+      };
     })
   );
 
@@ -38,6 +57,9 @@ function ManageLabelsUI({ queriedlabels }: Props) {
     target: { value: React.SetStateAction<string> };
   }) => {
     setSearch(event.target.value);
+  };
+  const handleDelete = () => {
+    console.info('You clicked the delete icon.');
   };
 
   let query = queriedlabels.map((l: any) => {
@@ -52,11 +74,31 @@ function ManageLabelsUI({ queriedlabels }: Props) {
     });*/
 
   function getLabellist() {
+    const _rgb = (color: Color) =>
+      'rgb(' + color.R + ',' + color.G + ',' + color.B + ')';
+
+    // Minimum contrast between the background and the text color
+    const contrastThreshold = 2.5;
+    // Guess the text color based on the background color
+    const getTextColor = (background: string) =>
+      getContrastRatio(background, common.white) >= contrastThreshold
+        ? common.white // White on dark backgrounds
+        : common.black; // And black on light ones
+
     const labelslist = searchLabel(labels, searchInput);
     return labelslist.map((label) => {
+      let labelcolor: Color = {
+        R: label.R,
+        G: label.G,
+        B: label.B,
+      };
+      const style = {
+        backgroundColor: _rgb(labelcolor),
+        color: getTextColor(_rgb(labelcolor)),
+      };
       return (
         <div>
-          <p>{label}</p>
+          <Chip label={label.name} onDelete={handleDelete} style={style} />
         </div>
       );
     });
