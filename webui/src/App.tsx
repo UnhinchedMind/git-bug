5fc83a85caf8eb01b71ffad4d9b5e26e5125ac03
Lena <lena.becker-3@mni.thm.de>
2021-03-12 00:55:47 +0100

    #12 Setup Identity Page

--- webui/src/App.tsx
+++ webui/src/App.tsx
@@ -3,6 +3,7 @@ import { Route, Switch } from 'react-router';
 
 import Layout from './components/Header';
 import BugPage from './pages/bug';
+import IdentityPage from './pages/identity/Identity';
 import ListPage from './pages/list';
 import NewBugPage from './pages/new/NewBugPage';
 
@@ -13,6 +14,7 @@ export default function App() {
         <Route path="/" exact component={ListPage} />
         <Route path="/new" exact component={NewBugPage} />
         <Route path="/bug/:id" exact component={BugPage} />
+        <Route path="/user" exact component={IdentityPage} />
       </Switch>
     </Layout>
   );
